0169a549a8db115783dcb20f0e9d0eac743c5ff5
OnePower <tgemmonoto@gmail.com>
2022-03-18 15:50:15 +0200

    some minor changes

--- uGridNet Start Packet/uGridNet_Revised_Optimized_Version_V3.py
+++ uGridNet Start Packet/uGridNet_Revised_Optimized_Version_V3.py
@@ -1763,7 +1763,7 @@ def SimulateNetwork(site_properties, conc_ID=None, min_trans=1):
     LV_kW = (230*130)/1000 # kW - LV line 
     min_num_trans = max(int(kW_max/LV_kW), min_trans, len(indexes_conn)//50)
     max_num_trans = min_num_trans + 5
-    household_current = kW_max*1000/(len(indexes_conn)*230)
+    household_current = 1.2*(kW_max*1000/(len(indexes_conn)*230))
     if household_current < 1.712262:
         household_current = 1.712262
     print(household_current)
