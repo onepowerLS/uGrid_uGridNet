9f9bb6a9ea7210f08c58169b74956d37e72e31ff
Teesmo <tgemmonoto@gmail.com>
2023-12-13 15:23:28 +0200

    ADD [requirements.txt] add dependency tracking file. MODIFY [constants.py] comment out unnecessary code ADD [TEST*] add testing spreadsheets

--- uGrid/constants.py
+++ uGrid/constants.py
@@ -1,7 +1,7 @@
 import sys 
 
-LOAD_FILE = 'LoadKW_MAK.xlsx'
-FULL_ENERGY_FILE =''
-TMY_FILE = ''
-INPUT_FILE = ''
+#LOAD_FILE = 'LoadKW_MAK.xlsx'
+#FULL_ENERGY_FILE =''
+#TMY_FILE = ''
+#INPUT_FILE = ''
 SITE_NAME = input('Enter area name: ')
