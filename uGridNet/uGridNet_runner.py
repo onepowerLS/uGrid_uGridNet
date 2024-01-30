32fb94172358d76c795ce30e51d8be8bb23aefd5
Teesmo <tgemmonoto@gmail.com>
2024-01-30 14:06:46 +0200

    Update uGridNet_runner.py

--- uGridNet/uGridNet_runner.py
+++ uGridNet/uGridNet_runner.py
@@ -892,7 +892,7 @@ def PoleElevation(gen_LON, gen_LAT, gen_indexes, target_indexes, d_EW_between, d
     DF = pd.DataFrame({'index_x': target_indexes[:, 0], 'index_y': target_indexes[:, 1],
                        'UTM_X': X - X_Shifts, 'UTM_Y': Y - Y_Shifts})
     new_GDF = gpd.GeoDataFrame(DF, geometry=gpd.points_from_xy(DF.UTM_X, DF.UTM_Y))
-    new_GDF = new_GDF.set_crs("EPSG:32631") #new_GDF.set_crs("EPSG:22289")  # EPSG:3857
+    new_GDF =  new_GDF.set_crs("WGS84") #new_GDF.set_crs("EPSG:32631") #new_GDF.set_crs("EPSG:22289")  # EPSG:3857
     #new_GDF = new_GDF.to_crs("WGS84")  # EPSG:4326
     gps = np.array([[list(i.coords)[0][0], list(i.coords)[0][1]] for i in new_GDF.geometry.values])
 
