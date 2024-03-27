9f18991523edf7569039320b56b198944ac54dd1
Teesmo <tgemmonoto@gmail.com>
2024-03-27 16:39:52 +0200

    Update uGridNet_runner.py

--- uGridNet/uGridNet_runner.py
+++ uGridNet/uGridNet_runner.py
@@ -857,6 +857,8 @@ def AngleBWPoints(P_END_1, P_Mid, P_END_2, d_EW_between, d_NS_between):
         theta2 = angle(P_Mid, P_END_2)
         angles = np.degrees([theta1, theta2])
         print(f'Angle associated values are theta1: {theta1}, theta2: {theta2}, angles: {angles}')
+    except AttributeError:
+        pass
     except TypeError:
         theta1 = float(0 if angle(P_Mid, P_END_1) is None else angle(P_Mid, P_END_1)) 
         theta2 = float(0 if angle(P_Mid, P_END_2) is None else angle(P_Mid, P_END_2))
