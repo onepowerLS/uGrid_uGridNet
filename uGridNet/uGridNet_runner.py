6bd2d47d4c1e50f3d5cd86251d891f75cbc7d4e8
Teesmo <tgemmonoto@gmail.com>
2024-03-27 15:42:35 +0200

    Update uGridNet_runner.py

--- uGridNet/uGridNet_runner.py
+++ uGridNet/uGridNet_runner.py
@@ -834,7 +834,7 @@ def AngleBWPoints(P_END_1, P_Mid, P_END_2, d_EW_between, d_NS_between):
         elif P2[1] == P1[1] and P2[0] < P1[0]:
             return np.pi
         else:
-            g = (P1[1] - P2[1]) * d_NS_between / ((P1[0] - P2[0]) * d_EW_between)
+            g = (0 if (P1[1] - P2[1]) * d_NS_between / ((P1[0] - P2[0]) * d_EW_between) is None else (P1[1] - P2[1]) * d_NS_between / ((P1[0] - P2[0]) * d_EW_between))
             print(f'The value of g is : {g}')
             theta = m.atan(g)
 
